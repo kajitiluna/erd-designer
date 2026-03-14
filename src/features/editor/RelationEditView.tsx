7258ad0db54c09d0c313cf6c69e241c6d5793fd0
Yoshiki Kajihara <yosshiki.kajiharara.1981@gmail.com>
2026-03-14 10:55:35 +0900

    Prevent selecting the same column for both parent and child in self-referencing relations.

--- src/features/editor/RelationEditView.tsx
+++ src/features/editor/RelationEditView.tsx
@@ -197,9 +197,10 @@ const RelationReferencesPanel = ({
             ) === false
         )
 
-        // 外部キー制約を指定できるのは、型定義が同一のカラムのみ
+        // 外部キー制約を指定できるのは、型定義が同一のカラムのみ。ただし、自己参照の場合、同じカラムは指定できない。
         const foreignDetails = childColumnDetails.filter(childPair =>
-            childPair.columnShareModel.matchForReferenceType(parentColumnShareModel)
+            childPair.columnShareModel.matchForReferenceType(parentColumnShareModel) &&
+            (primaryColumn.columnModelId !== childPair.columnModel.columnModelId)
         );
 
         const labelId = `label-${primaryColumn.columnShareModelId}`
